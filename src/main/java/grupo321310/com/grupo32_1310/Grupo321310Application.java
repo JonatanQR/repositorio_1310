96f73f1f177831b1cb7d7a17c1cc919df54f7bee
JonatanQR <jonatan.quinones.mt@correo.usa.edu.co>
2021-10-14 09:57:12 -0500

    springboot

---
+++ src/main/java/grupo321310/com/grupo32_1310/Grupo321310Application.java
@@ -0,0 +1,13 @@
+package grupo321310.com.grupo32_1310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo321310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo321310Application.class, args);
+	}
+
+}
---
+++ src/test/java/grupo321310/com/grupo32_1310/Grupo321310ApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo321310.com.grupo32_1310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo321310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
